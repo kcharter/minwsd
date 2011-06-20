923e123aa33adae0e32e7d874e5919632c68c6a0
Kevin Charter <kevin.charter@acm.org>
2011-06-19 22:59:57 -0600

    Rework MinWSDiff.minWSDiff.
    Rework 'minWSDiff' by
    
    - diffing lists of word texts, instead of WordOrWS
    
    - reinserting non-words, giving preference to the non-words
      from the old version, except for trailing non-words
    
    At this point, the unit tests pass. The exception of preferring
    the trailing non-words from the new version permits one
    of the unit tests (diff of an empty sequence with X is X) to
    pass. I may change the test, but not as part of this commit.
    
    At this point, minwsd discards comments that appear only in
    the original version, since comments appear to be sequences
    of words unique to the original, and so are discarded. In
    subsequent commits, I'll make comments into a special type
    of non-word token, and require recognizing them in the
    parser, perhaps with a language-specific option.
    
    Unfortunately, the diff for this commit is rather nasty
    to read; I ended up changing 'precedingNonWords' into
    'nonWordGroups' and dropping the word positions. I also
    deleted the now-unused 'extractLeadingWS' function, and
    removing the now-unused EqWS newtype.

--- MinWSDiff.hs
+++ MinWSDiff.hs
@@ -1,20 +1,13 @@
 module MinWSDiff where
 
 import Data.Algorithm.Diff
+import Data.Maybe (catMaybes)
 import qualified Data.Text as T
 
 import Parse
 import Unparse
 import WordsAndWS
 
-
--- | We use a newtype in order to impose an alternative equality
--- where all white space is considered equivalent.
-newtype EqWS = EqWS WordOrWS deriving (Show)
-
-instance Eq EqWS where
- (EqWS (WS _)) == (EqWS (WS _)) = True
- (EqWS w1) == (EqWS w2) = w1 == w2
 
 -- | Given an old text and a new text, return a text with the new
 -- words but whitespace as similar as possible to the old text.
@@ -27,40 +20,30 @@ minimizeWhitespaceDiffs oldText newText =
 -- the same order, but as much of the old whitespace as possible.
 minWSDiff :: [WordOrWS] -> [WordOrWS] -> [WordOrWS]
 minWSDiff old new =
-  let diff = getDiff (toEqWS old) (toEqWS new)
-      toEqWS = map EqWS
-      replaceWS diff old new = replaceWS' [] diff old new
-      replaceWS' sofar diff old new =
+  let diff = getDiff (wordTexts old) (wordTexts new)
+      wordTexts = catMaybes . map wordText
+      replaceWS diff oldNWs newNWs = replaceWS' [] diff oldNWs newNWs
+      replaceWS' sofar diff oldNWs newNWs =
         case diff of
-          (F, EqWS x):diff' ->
-            case x of
-              Word _ -> replaceWS' sofar diff' (tail old) new
-              WS _   -> replaceWS' (x:sofar) diff' (tail old) new
-          (S, EqWS x):diff' ->
+          (F, _):diff' ->
+            replaceWS' sofar diff' (tail oldNWs) newNWs
+          (S, x):diff' ->
             -- we preserve both words and white space unique to the
             -- new stream; if it shows up in the diff, it is likely
             -- before a new word
-            replaceWS' (x:sofar) diff' old (tail new)
-          (B, EqWS x):diff' ->
-            case x of
-              Word _ -> replaceWS' (x:sofar) diff' (tail old) (tail new)
-              WS _   -> let (y, old') = extractLeadingWS old
-                        in replaceWS' (y:sofar) diff' old' (tail new)
-          [] -> reverse $ sofar
-      extractLeadingWS (y@(WS _):rest) = (y, rest)
-      extractLeadingWS (x:rest) = error $ "Expected whitespace, but got '" ++ asString x ++ "'."
-      extractLeadingWS [] = error $ "Expected whitespace, but list is empty."
-      asString = T.unpack . content
-  in replaceWS diff old new
+            replaceWS' (Word x:(reverse (head newNWs) ++ sofar)) diff' oldNWs (tail newNWs)
+          (B, x):diff' ->
+            replaceWS' (Word x:(reverse (head oldNWs) ++ sofar)) diff' (tail oldNWs) (tail newNWs)
+          [] -> reverse $ (reverse (head newNWs) ++ sofar)
+  in replaceWS diff (nonWordGroups old) (nonWordGroups new)
 
 
--- | A list of pairs of zero-based word positions and lists of
--- preceding non-words.
-precedingNonWords :: [WordOrWS] -> [(Int, [WordOrWS])]
-precedingNonWords = foldr accum [] . zip [0..]
-  where accum (i,x) sofar
-          | isWord x  = (i,[]):sofar
-          | otherwise = (case sofar of
-                            [] -> []
-                            ((i,nws):rest) -> ((i,x:nws):rest))
+-- | A list of lists of non-words. All but the last are the possibly
+-- empty consecutive non-words preceding a word. The last is the
+-- possibly empty sequence of trailing non-words
+nonWordGroups :: [WordOrWS] -> [[WordOrWS]]
+nonWordGroups = foldr accum [[]]
+  where accum x sofar
+          | isWord x  = []:sofar
+          | otherwise = (x:head sofar):tail sofar
 
