a0ed3fd6b0e504f18d97de7b7523085e3e21f249
Kevin Charter <kevin.charter@acm.org>
2011-06-25 18:13:30 -0600

    Extract the 'tokenizeHandle' function in 'minwsd'.

--- minwsd.hs
+++ minwsd.hs
@@ -4,7 +4,7 @@ import Control.DeepSeq (deepseq)
 import Control.Monad (unless)
 import System.Environment
 import System.Exit
-import System.IO (withFile, hGetContents, IOMode(..))
+import System.IO (Handle, withFile, hGetContents, IOMode(..))
 
 import Parse
 import Unparse
@@ -19,13 +19,16 @@ main = do
   wws1 <- tokenizeFile f1
   wws2 <- tokenizeFile f2
   putStr $ unparse $ minWSDiff wws1 wws2
-  
+
 tokenizeFile :: FilePath -> IO [Token]
 tokenizeFile f =
-  withFile f ReadMode $ \h -> do
-    text <- hGetContents h
-    let p = parse text in p `deepseq` return p
+  withFile f ReadMode tokenizeHandle
   
+tokenizeHandle :: Handle -> IO [Token]
+tokenizeHandle h = do
+  text <- hGetContents h
+  let p = parse text in p `deepseq` return p
+
 printUsageAndExit = do
   progName <- getProgName
   putStrLn $ "Usage: " ++ progName ++ " <old file> <new file>"
