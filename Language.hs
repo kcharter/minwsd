f0c26f7f15fd593a7174235816cf62776716d3aa
Kevin Charter <kevin.charter@acm.org>
2011-06-25 19:57:00 -0600

    Remove the 'Parse' and 'Unparse' modules.
    The 'Language' module is the only one that depends on the
    'Parse' and 'Unparse' modules. Moreover, the 'Unparse' type
    class defined in 'Unparse' is unnecessary. I've moved the
    code from 'Parse' and 'Unparse' into language, and removed
    the original modules. The 'Unparse' type class is now
    gone, and 'defaultUnparse' has the obvious one-line definition.

--- Language.hs
+++ Language.hs
@@ -1,8 +1,9 @@
 module Language where
 
+import Data.Char
+import qualified Data.Text as T
+
 import Tokens
-import qualified Parse as P
-import qualified Unparse as U
 
 -- | Support for a particular language or family of languages.
 data Language = Language {
@@ -12,11 +13,32 @@ data Language = Language {
   unparser :: [Token] -> String
   }
 
--- | Default language support, equivalent to ordinary text
--- with no comments. For most programming languages, you
+-- | Default language support, with the 'plainTextParse' parser and
+-- the 'defaultUnparse' unparser. For most programming languages, you
 -- can simply replace the parser.
 defaultLanguage :: Language
 defaultLanguage = Language {
-  parser = P.parse,
-  unparser = U.unparse
+  parser = plainTextParse,
+  unparser = defaultUnparse
   }
+
+-- | A plain text parser. Every non-empty sequence of non-whitespace
+-- characters becomes a 'Word', and every non-empty sequence of
+-- whitespace characters becomes a 'WS'. This parser never produces a
+-- 'Comment'.
+plainTextParse :: String -> [Token]
+plainTextParse [] = []
+plainTextParse (c:s) =
+  let (w, s') =
+        if isSpace c
+        then let (spaces, s') = span isSpace s
+             in (WS (T.pack (c:spaces)), s')
+        else let (word, s') = break isSpace s
+             in (Word (T.pack (c:word)), s')
+  in w:plainTextParse s'
+
+-- | Concatentates the contents of all the tokens, and inserts no
+-- other characters. As long as your parser puts distributes all the
+-- characters in the input among tokens, you can use this unparser.
+defaultUnparse :: [Token] -> String
+defaultUnparse = concatMap (T.unpack . content)
