767ee8797c8798762d0646508fffae95ca265800
Kevin Charter <kevin.charter@acm.org>
2011-06-25 19:37:45 -0600

    Tests: prepare for more than one 'Langauge'.
    The 'Tests' module was biased toward the parser for plain text.
    In particular,
    
    - the Arbitrary instance generated tokens that could be
      produced by the plain text parser
    
    - the 'prop_unparseParse' property really only applies to
      plain text tokens
    
    - the 'minimize' function also only applies to plain text
      tokens
    
    I've turned the old 'arbitrary' generator into 'plainTextTokens',
    and redefined 'arbitrary' so that the text in Words, WS, and
    Comments is completely unrestricted. The correct operation of
    'minWSDiff' doesn't depend on the particulars of the contents
    of tokens, the new 'arbitrary' generator still works with the
    properties
    
       prop_minWSDiffXXIsX
       prop_minWSDiffEmptyXIsX
       prop_minWSDiffContainsJustNewWords
    
    Property 'prop_unparseParse' is now 'prop_plainTextUnparseParse',
    and uses the 'defaultLanguage' definition rather than directly
    importing the Parse and Unparse modules.
    
    Finally, 'minimize' is now 'minimizePlainText', since it assumes
    the token contents are consistent with a plain text parse.
    
    After these changes, the tests still pass.

--- Test.hs
+++ Test.hs
@@ -4,43 +4,57 @@ import Data.Char
 import qualified Data.Text as T
 import Test.QuickCheck
 
-import Tokens
-import Parse
-import Unparse
 import MinWSDiff
+import Language
+import Tokens
 
 main :: IO ()
 main = do
-  quickCheck prop_unparseParse
+  quickCheck $ forAll (listOf plainTextTokens) $ prop_plainTextUnparseParse
   quickCheck prop_minWSDiffXXIsX
   quickCheck prop_minWSDiffEmptyXIsX
   quickCheck prop_minWSDiffContainsJustNewWords
 
 instance Arbitrary Token where
   arbitrary =
-    oneof [(Word . T.pack) `fmap` (listOf1 $ arbitrary `suchThat` (not . isSpace)),
-           (WS . T.pack) `fmap` (listOf1 $ elements " \t\r\n\f")]
+    oneof [(Word . T.pack) `fmap` arbitrary,
+           (WS . T.pack) `fmap` arbitrary,
+           (Comment . T.pack) `fmap` arbitrary]
 
-prop_unparseParse :: [Token] -> Bool
-prop_unparseParse wordsOrWs =
-  minimize wordsOrWs == (parse . unparse) wordsOrWs
+plainTextTokens :: Gen Token
+plainTextTokens =
+  oneof [(Word . T.pack) `fmap` (listOf1 $ arbitrary `suchThat` (not . isSpace)),
+         (WS . T.pack) `fmap` (listOf1 $ elements " \t\r\n\f")]
+  
+prop_plainTextUnparseParse :: [Token] -> Bool
+prop_plainTextUnparseParse wordsOrWs =
+  minimizePlainText wordsOrWs == (parse . unparse) wordsOrWs
+  where parse = parser defaultLanguage
+        unparse = unparser defaultLanguage
 
-minimize :: [Token] -> [Token]
-minimize (w1:w2:rest) =
+minimizePlainText :: [Token] -> [Token]
+minimizePlainText (w1:w2:rest) =
   case w1 of
     Word t1 ->
       case w2 of
         Word t2 ->
-          minimize (Word (T.append t1 t2):rest)
+          minimizePlainText (Word (T.append t1 t2):rest)
         WS _ ->
-          w1:minimize (w2:rest)
+          w1:minimizePlainText (w2:rest)
+        Comment _ ->
+          unexpectedComment
     WS s1 ->
       case w2 of
         Word _ ->
-          w1:minimize (w2:rest)
+          w1:minimizePlainText (w2:rest)
         WS s2 ->
-          minimize $ WS (T.append s1 s2):rest
-minimize s = s
+          minimizePlainText $ WS (T.append s1 s2):rest
+        Comment _ ->
+          unexpectedComment
+    Comment _ ->
+      unexpectedComment
+    where unexpectedComment = error "There shouldn't be comments in 'plain text'."
+minimizePlainText s = s
 
 prop_minWSDiffXXIsX :: [Token] -> Bool
 prop_minWSDiffXXIsX wws = wws == minWSDiff wws wws
